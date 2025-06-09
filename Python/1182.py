ac476145af762d98d394bd8e0be7ccfa971cd155
AndersonDuarte-png <anderson.duarte889@gmail.com>
2025-06-09 00:46:47 -0300

    add resolucao do item 1182

---
+++ Python/1182.py
@@ -0,0 +1,21 @@
+C = int(input())
+T = input()
+array = []
+
+for x in range(12):
+    arr = []
+    for y in range(12):
+        arr.append(input())
+    array.append(arr)
+if T == "S":
+    Soma = float (0)
+    for x in range(12):
+        Soma = float(array[int(x)][C]) + float(Soma)
+    print("{}".format(Soma))
+
+elif T == "M":
+    Soma = float (0)
+    for x in range(12):
+        Soma = float(array[int(x)][C]) + float(Soma)
+    Media = float(Soma)/12
+    print("{:.1f}".format(Media))
