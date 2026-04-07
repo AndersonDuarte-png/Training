a975efd045da552af2ada35eaf2d20d8774a7037
Anderson Duarte <anderson.duarte889@gmail.com>
2026-04-06 22:11:30 -0300

    add POST - monthly_payment, total_payment, total_interest

--- Loan Simulation API/main.py
+++ Loan Simulation API/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 
 @app.post("/simulate")
 def simulate_loan(request: LoanRequest):
-    return request
+    return{
+        "monthly_payment":round(request.monthly_payment(),2),
+        "total_payment":round(request.calculateTotal_Payment(),2),
+        "total_interest":round(request.calculateTotal_interest(),2),
+    }
 
 @app.get("/health")
 def read_root():
--- Loan Simulation API/models.py
+++ Loan Simulation API/models.py
@@ -6,9 +6,23 @@ class LoanRequest(BaseModel):
     interest_rate: float
     months: int
 
+
+
     @field_validator("amount","interest_rate","months")
     @classmethod
+    
+    
     def validate_positive(cls,value):
         if value <= 0:
                 raise ValueError("Apenas valores maior que zero")
         return value
+    
+    def calculateTotal_interest(self):
+         return float(self.calculateTotal_Payment() - self.amount)
+    
+    def calculateTotal_Payment(self):
+        return float(self.monthly_payment()  * self.months)
+    
+    def monthly_payment(self):
+        monthly_payment = self.amount * (self.interest_rate * (1 + self.interest_rate)**self.months) / ((1 + self.interest_rate)**self.months - 1)
+        return monthly_payment
